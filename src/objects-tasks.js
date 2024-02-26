8f84abf48bce15b676e7e0cf2f6269980bd3ac3e
Kirill <kiw197204@gmail.com>
2024-02-26 09:47:41 +0300

    feat: mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,16 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const res = {};
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach((el) => {
+      const [key, value] = el;
+      if (res[key] === undefined) res[key] = 0;
+      res[key] += value;
+    });
+  });
+  return res;
 }
 
 /**
